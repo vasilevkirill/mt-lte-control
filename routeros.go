abd8424ccc56cfebdac55bd0d3c9b985e4ac379d
vasilev.k <katpeknacziN9guphi>
2023-02-13 00:51:55 +0300

    init

--- routeros.go
+++ routeros.go
@@ -14,4 +14,20 @@ func getInfoOverApi() (interface{}, error) {
 		return nil, err
 	}
 	r, err := conn.RunArgs(ar)
+
+	return nil, nil
+}
+
+func sendCommand(command []string) (interface{}, error) {
+	timeout := 4 * time.Second
+	connString := fmt.Sprintf("%s:%d", Config.GetString("router.ip"), Config.GetInt("router.apiport"))
+	conn, err := routeros.DialTimeout(connString, Config.GetString("router.user"), Config.GetString("router.password"), timeout)
+	if err != nil {
+		return nil, err
+	}
+	r, err := conn.RunArgs(command)
+	if err != nil {
+		return nil, err
+	}
+	return r.Re, nil
 }
